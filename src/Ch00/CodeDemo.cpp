5dfe7f18c645202661d08c557c2ca2d1da7c309d
khs00667 <khs00667@gmail.com>
2025-10-01 05:00:07 +0000

    Append a 3rd variable!

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,18 +1,21 @@
 // Learning C++ 
 // Exercise 00_03
 // Using the exercise files on GitHub, by Eduardo Corpeño 
+// Append with a 3rd variable!
 
 #include <iostream>
 
 int main(){
-    float num_1, num_2, result;
+    float num_1, num_2, num_3, result;
 
     std::cout << "Enter number 1: " << std::flush;
     std::cin >> num_1;
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
-    
-    result = num_1 + num_2;
+    std::cout << "Enter number 3: " << std::flush;
+    std::cin >> num_3;
+   
+    result = num_1 + num_2 + num_3;
     
     std::cout << "The result of the addition is " << result << std::endl;
     
